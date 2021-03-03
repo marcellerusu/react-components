ad8dd1df67707cc8d163738ec644275e10619ecf
MarcelRusu <marcelrusu@protonmail.com>
2021-03-03 18:43:47 -0500

    broken stuff

--- src/libUI/index.js
+++ src/libUI/index.js
@@ -1,118 +1,102 @@
-let idCounter = 0;
-const states = {};
-
-export const mount = (component, elem) =>
-  elem.appendChild(render(component));
-
-const morphAttrs = (oldElem, newElem, id) => {
-  // in future do diffs
-  for (let prop of newElem.attributes) {
-    oldElem.setAttribute(prop.name, prop.value);
-  }
-  // in future do diffs
-  for (let [prop, value] of Object.entries(states[id]?.domProps || {})) {
-    oldElem[prop] = value;
-  }
-  // assuming same # of children
-  for (let i = 0; i < newElem.children.length; i++) {
-    morphAttrs(oldElem.children[i], newElem.children[i]);
-  }
-};
-
-const morph = (oldElem, newElem) => {
-  // 1. should be a dom diff..??
-  oldElem.parentElement.replaceChild(newElem, oldElem);
-  // morphAttrs(oldElem, newElem, id);
-  // if (blah) render text?
-}
-
-const updateState = (newValue, state) => {
-  if (typeof newValue === 'function') {
-    return newValue(state);
-  } else {
-    return newValue;
-  }
-};
-
-// TODO: take out useEffect
-export const withState = component => {
-  const id = idCounter++;
-  states[id] = states[id] || {states: [], props: undefined};
-  let stateCounter = 0;
-  const useState = initialValue => {
-    states[id].states[stateCounter] = states[id].states[stateCounter] || initialValue;
-    const setState = currentStateCounter => newValue => {
-      states[id].states[currentStateCounter] = updateState(newValue, states[id].states[currentStateCounter]);
-      const newElem = render(() => {
-        stateCounter = 0;
-        return () => component({...states[id].props, useState})(id);
-      });
-      const oldElem = document.querySelector(`[data-lib-ui-id="${id}"]`);
-      morph(oldElem, newElem);
-    }
-    stateCounter++;
-    return [states[id].states[stateCounter - 1], setState(stateCounter - 1)];
-  };
-
-  return props => {
-    states[id].props = states[id].props || props;
-    stateCounter = 0;
-    return () => component({...states[id].props, useState})(id);
-  };
-};
-
-const render = (elem) => {
-  switch (typeof elem) {
-    case 'number':
-    case 'string':
-    case 'undefined':
-      return document.createTextNode(elem ?? '');
-    case 'function':
-      return render(elem());
-    case 'object': // TODO check if HTMLElement directly
-      return elem;
-  }
-};
-
-// {
-//   state: '3',
-//   domProps: 2,
-//   domChildren: [
-
+// const state = {
+//   _id: 0,
+//   componentProps: {},
+//   state: ['2', [{email: 'marcelle@rusu.com'}]],
+//   type: 'custom',
+//   domProps: null,
+//   children: [
+//     { 
+//       componentProps: {},
+//       state: null,
+//       type: 'div', // either dom type | 'custom' | 'primitive',
+//       domProps: {},
+//       children: [
+//         {
+//         }
+//       ]
+//     }
 //   ]
 // }
 
-// start using shadow dom
 
-// morph(old, new) = 
-//   // diffprops checks html props & lib-ui props
-//   if (diffProps(old, new)) {
-//     morphProps(old, new);
-//     newChildren, oldChildren
-//     newChildren.zip(oldChildren)
-//       .forEach(morph);
-//   }
+let idCounter = 0;
+export const Component = component => {
+  return componentProps => ({
+    _id: idCounter++,
+    componentProps,
+    component,
+    state: [], // done at run time
+    type: 'custom',
+    domProps: null,
+    children: []
+  });
+};
+
+export const mount = (component, root) => (
+  console.log(render(component)[0]),
+  root.appendChild(render(component)[0])
+);
+
+const render = element => {
+  console.log('render', element);
+  switch (element.type) {
+    // reached base case
+    case 'primitive':
+    case undefined:
+      console.log({element});
+      return document.createTextNode(element);
+    // still a component
+    case 'custom':
+      // probably needs to track state here
+      const component = element.component(element.componentProps);
+      console.log('component', component)
+      if (component.type !== 'custom') {
+        element.children = component.children;
+      } else {
+        element.children = render(component);
+      }
+      return element.children.map(render);
+    // dom element
+    default:
+      return renderDomNode(element);
+  }
+};
 
 export const dom = new Proxy({}, {
-  get(target, prop, receiver) {
-    return props => (...contents) => {
-      return id => {
-        const elem = document.createElement(prop);
-        if (id !== undefined) states[id].domProps = states[id].domProps || {};
-        for (let key in props) {
-          if (key === 'className') {
-            elem.classList.add(props[key]);
-          } else {
-            elem[key.toLowerCase()] = props[key];
-          }
-          if (id !== undefined) states[id].domProps[key.toLowerCase()] = props[key];
-        }
-        for (const content of contents) {
-          elem.appendChild(render(content));
-        }
-        if (id !== undefined) elem.dataset['libUiId'] = id;
-        return elem;
-      };
-    };
+  get(_, domType) {
+    return domProps => (...children) =>
+      ({type: domType, domProps, children});
   }
 });
+
+const renderDomNode = ({type, domProps, children}) => {
+  const elem = document.createElement(type);
+  for (const key in domProps) {
+    if (key === 'className') {
+      elem.classList.add(domProps[key]);
+    } else {
+      elem[key.toLowerCase()] = domProps[key];
+    }
+  }
+  // console.log({type, domProps, children})
+  for (const child of children) {
+    elem.appendChild(render(child));
+  }
+  return elem;
+};
+
+
+
+const Header = Component(({name}) => (
+  dom.header()(name)
+));
+
+const App = Component(() => (
+  Header({name: 'Hello World!'})
+));
+
+const state = App();
+
+mount(state, document.getElementById('root'));
+
+console.log(state);
